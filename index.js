d9f7795f6fcb0831731c40f1c271839ad7666ed3
fisker <lionkay@gmail.com>
2016-05-04 16:13:59 +0800

    1 catch node-sass not installed error 2 add autoprefixer options 3 cerrect misspelled "compresse"

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var globalModulesPath = process.env.APPDATA + "\\npm\\node_modules\\";
 var fs = require("fs");
 var path = require("path");
 
-var sassParser = require(globalModulesPath + "node-sass");
 var postcss = require('postcss');
 var autoprefixer = require('autoprefixer');
 
@@ -14,18 +13,23 @@ var options = {
     outputfile : args[1],
     outputStyle : args.indexOf("--compresse") > -1 ? "compressed" : "expanded",
     sourceMap : args.indexOf("--sourceMap") > -1 ? true : false
+};
+
+var sassParser;
+try {
+  sassParser = require(globalModulesPath + "node-sass");
+} catch(e) {
+  console.error('run `npm i -g node-sass` to install `node-sass` module first');
 }
 
-
 // node-sass render
-sassParser.render({
+sassParser && sassParser.render({
         file: options.inputfile,
         outFile: options.outputfile,
         outputStyle : options.outputStyle,
         sourceMap : options.sourceMap
         //[, options..]
-    }, 
-    (err, result) => {
+    }, function(err, result) {
         if(err) console.error("error:", err);
         var css = result.css,
             map = options.sourceMap ? result.map.toString("utf8") : false;
@@ -38,17 +42,26 @@ sassParser.render({
                     sourcesContent: false,
                     prev: map
                 } : false
-        }
+        };
+
+        // see https://github.com/postcss/autoprefixer
+        var autoprefixerOpt = {
+          browsers: []
+        };
+
         // postcss process
-        postcss([autoprefixer]).process(css, postcssOpt).then((postcssResult)=>{
-                fs.writeFile(options.outputfile, postcssResult.css, "utf8");
-                console.log("output \"", options.inputfile, "\" success");
-                if(map) {
-                    fs.writeFile(options.outputfile + ".map", postcssResult.map, "utf8");
-                    console.log("output \"", options.outputfile + ".map", "\" success");
-                }
-            }
-        );
-        
+        postcss([autoprefixer(autoprefixerOpt)])
+          .process(css, postcssOpt)
+          .then(function(postcssResult) {
+                  fs.writeFile(options.outputfile, postcssResult.css, "utf8");
+                  console.log("output \"", options.inputfile, "\" success");
+                  if(map) {
+                      fs.writeFile(options.outputfile + ".map", postcssResult.map, "utf8");
+                      console.log("output \"", options.outputfile + ".map", "\" success");
+                  }
+              }
+          ).catch(function(error){
+            console.error('error:', error);
+          });
     }
 );
